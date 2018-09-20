c51634c8b03642c93890cf8a9992a38e5a3df64b
Piepgras <thepiepgras@gmail.com>
2018-09-20 19:17:41 +0200

    Made simple level, added "player" and simple movement + prepared for animation.

---
+++ CodenameNUTMEG/Assets/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D c;
+    public float runSpeed = 20f;
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonDown("Up"))
+        {
+            crouch = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        c.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
