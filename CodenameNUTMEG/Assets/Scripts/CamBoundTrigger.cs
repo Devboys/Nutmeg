dfbc96e8c654cf9c519954a8c37e94c04f38c85d
Jeppe Faber <JeppestougaardFaber@gmail.com>
2018-10-21 22:48:36 +0200

    Implemented basic camera bounding mechanism
    Camera can now be blocked by so-called Camera-blockers, which will lock the camera until the player moves away again.

---
+++ CodenameNUTMEG/Assets/Scripts/CamBoundTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamBoundTrigger : MonoBehaviour {
+
+    public delegate void blockDirectionDelegate(bool b, Collider2D c);
+    public blockDirectionDelegate blockDelegate;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "CameraBlock")
+        {
+            blockDelegate(true, collision);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "CameraBlock")
+        {
+            blockDelegate(false, collision);
+        }
+    }
+}
--- CodenameNUTMEG/Assets/Scripts/PointCamFollow.cs
+++ CodenameNUTMEG/Assets/Scripts/PointCamFollow.cs
@@ -8,56 +8,134 @@ public class PointCamFollow : MonoBehaviour
     public float smoothSpeedX = 0.1f;
     public float smoothSpeedY = 0.1f;
     public float gizmoRadius = 0.1f;
+    public bool cameraBoundsOn = false;
+    public GameObject boundPrefab;
 
     FocusArea focusArea;
 
+    GameObject upperBound;
+    GameObject lowerBound;
+    GameObject rightBound;
+    GameObject leftBound;
+
+    readonly float boundThickness = 0.5f;
+
+    bool blockedX;
+    bool blockedY;
+
     void Start()
     {
-        focusArea = new FocusArea(target.GetComponent<Collider2D>().bounds.center, smoothSpeedX, smoothSpeedY);
+        //create camera focus area
+        focusArea = new FocusArea(target.GetComponent<Collider2D>().bounds.center, smoothSpeedX, smoothSpeedY, cameraBoundsOn);
+
+        //create bounding colliders(used for limiting camera movement). Bounds are tied to the focusarea.
+        RespawnBounds();
     }
 
     void FixedUpdate()
     {
-
         focusArea.Update(target.GetComponent<Collider2D>().bounds.center);
-        Vector3 focusPosition = focusArea.center; //+ Vector2.up * verticalOffset;
+        Vector3 focusPosition = focusArea.areaObject.transform.position;
 
-        transform.position = focusPosition + Vector3.forward * -10;
+        if (!blockedX)
+            transform.position = new Vector3(focusPosition.x, transform.position.y, -10);
+        if (!blockedY)
+            transform.position = new Vector3(transform.position.x, focusPosition.y, -10);
     }
 
     void OnDrawGizmos()
     {
-        Gizmos.color = new Color(1, 0, 0);
-        Gizmos.DrawWireSphere(focusArea.center, gizmoRadius);
+        if (focusArea != null)
+        {
+            Gizmos.color = new Color(1, 0, 0);
+            Gizmos.DrawWireSphere(focusArea.areaObject.transform.position, gizmoRadius);
+        }
     }
 
-    struct FocusArea
+    void RespawnBounds()
     {
-        public Vector3 center;
+        //destroy current bound if it exist.
+        if (upperBound != null)
+            Destroy(upperBound);
+
+        //Create bounding camera collider.
+        upperBound = Instantiate(boundPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+
+        //make bound child of the focusarea to make its position relative to this at all times.
+        upperBound.transform.parent = focusArea.areaObject.transform;
+
+        //calculate the current screen size.
+        Vector2 screenSize = getOrthographicCameraSize();
+
+        //resize and position bound to fit the screen.
+        upperBound.transform.position = new Vector3(0, 0, 0);
+        upperBound.transform.localScale = new Vector3(screenSize.x * 2, screenSize.y * 2, upperBound.transform.localScale.z);
+
+        //delegate block responsibility
+        upperBound.GetComponent<CamBoundTrigger>().blockDelegate = setBlocked;
+
+
+    }
+
+    Vector2 getOrthographicCameraSize()
+    {
+        float screenX = Camera.main.orthographicSize * Screen.width / Screen.height;
+        float screenY = Camera.main.orthographicSize;
+        return new Vector2(screenX, screenY);
+    }
+
+    private class FocusArea
+    {
+        Vector3 center;
         float speedX;
         float speedY;
 
-        public FocusArea(Vector3 areaCenter, float smoothSpeedX, float smoothSpeedY)
+        public bool blockedX;
+        public bool blockedY;
+        public bool boundsOn;
+
+        public GameObject areaObject;
+
+        public FocusArea(Vector3 areaCenter, float smoothSpeedX, float smoothSpeedY, bool _boundsOn)
         {
             speedX = smoothSpeedX;
             speedY = smoothSpeedY;
             center = areaCenter;
+
+            blockedX = false;
+            blockedY = false;
+            boundsOn = _boundsOn;
+
+            areaObject = new GameObject();
         }
 
         public void Update(Vector3 newCenter)
         {
-
             //calculate camera velocity from distance between camera and collider center.
             float velocityX = (newCenter.x - center.x) * speedX;
             float velocityY = (newCenter.y - center.y) * speedY;
+
             Vector3 velocity = new Vector3(velocityX, velocityY, 0);
-
             center += velocity;
-        }
 
-        public void SetCenter(Vector3 c)
-        {
-            center = c;
+            areaObject.transform.position = center;
         }
+    }
+
+    public void setBlockedX(bool b, Collider2D source)
+    {
+        blockedX = b;
+    }
+    public void setBlocked(bool b, Collider2D source)
+    {
+
+        //get distance x and y.
+        float distX = Mathf.Abs(this.transform.position.x - source.transform.position.x);
+        float distY = Mathf.Abs(this.transform.position.y - source.transform.position.y);
+
+        if (distX >= distY)
+            blockedX = b;
+        else
+            blockedY = b;
     }
 }
