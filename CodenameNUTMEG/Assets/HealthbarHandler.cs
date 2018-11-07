bb01c4488175ae216b51a052cd783f7ddd0a315a
Jeppe Faber <JeppestougaardFaber@gmail.com>
2018-11-07 19:38:26 +0100

    Laid groundwork for damage system & simple UI

---
+++ CodenameNUTMEG/Assets/HealthbarHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthbarHandler : MonoBehaviour {
+
+    [SerializeField] PlayerHealthHandler playerHealthScript;
+    [SerializeField] GameObject heartPrefab;
+
+    List<GameObject> hearts = new List<GameObject>();
+
+    private readonly int heartSpriteSize = 35;
+
+    private void Start()
+    {
+        for(int i = 0; i < playerHealthScript.GetInitHealth(); i++)
+        {
+            GameObject newHeart = Instantiate(heartPrefab);
+
+            newHeart.GetComponent<RectTransform>().SetParent(this.GetComponent<RectTransform>());
+
+            newHeart.GetComponent<RectTransform>().anchoredPosition = new Vector2((heartSpriteSize * hearts.Count) - heartSpriteSize, 0);
+
+            hearts.Add(newHeart);
+        }
+
+        playerHealthScript.onHealthChangedEvent += UpdateHeartDisplay;
+    }
+
+    public void UpdateHeartDisplay(int totalNumHearts)
+    {
+        //empty heart list.
+        for(int i = hearts.Count; i > 0; i--)
+        {
+            Destroy(hearts[i - 1]);
+            hearts.RemoveAt(i - 1);
+        }
+
+        //fill heart list.
+        for (int i = 0; i < totalNumHearts; i++)
+        {
+            GameObject newHeart = Instantiate(heartPrefab);
+
+            newHeart.GetComponent<RectTransform>().SetParent(this.GetComponent<RectTransform>());
+
+            newHeart.GetComponent<RectTransform>().anchoredPosition = new Vector2((heartSpriteSize * hearts.Count) - heartSpriteSize, 0);
+
+            hearts.Add(newHeart);
+        }
+    }
+
+    
+
+}
---
+++ CodenameNUTMEG/Assets/Scripts/PlayerHealthHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthHandler : MonoBehaviour {
+
+    [SerializeField] private int initHealth = 3;
+
+    public event Action<int> onHealthChangedEvent;
+
+
+    public int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = initHealth;
+    }
+
+    public void ModHealth(int amount)
+    {
+        currentHealth += amount;
+
+        if(onHealthChangedEvent != null)
+        {
+            onHealthChangedEvent(currentHealth);
+        }
+
+        if(currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        //temporary die
+        transform.position = Vector3.zero;
+        ModHealth(initHealth);
+    }
+
+    public int GetInitHealth()
+    {
+        return initHealth;
+    }
+}
+
